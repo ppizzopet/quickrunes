7e8ec1431ce828d3f76a70eb898e6b41a628f214
Peter <77161701+ppizzopet@users.noreply.github.com>
2021-04-24 10:45:36 +0200

    Update update.py

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ try:
     import time
     from zipfile import ZipFile
 
-    def download_url(url, save_path, chunk_size=128):
+    def download_url(url, save_path, chunk_size=200000000):
         r = get(url, stream=True)
         with open(save_path, 'wb') as fd:
             for chunk in r.iter_content(chunk_size=chunk_size):
@@ -16,13 +16,13 @@ try:
         data = request.json()
         return str(data)
 
-
     newestVer = checkVersion()
     updatefilesurl = f"http://www.github.com/ppizzopet/quickrunes/releases/download/{newestVer}/update.zip"
 
     print("QuickRunes Updater v1.1")
     print("")
     quickrunesfilespath = input("Enter QuickRunes files path (C:/path/to/files/): ")
+    os.chmod(quickrunesfilespath, 777)
     print("Downloading update...")
     download_url(updatefilesurl, f"{quickrunesfilespath}update.zip")
     print("Unpacking file...")
